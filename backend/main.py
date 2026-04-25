e893bc8e7b7bc132b51cfa1e510440d53fdbb23b
Jyotishmoi Sarma <jyotishmoisarma8@gmail.com>
2026-04-25 20:16:20 +0530

    *

--- backend/main.py
+++ backend/main.py
@@ -13,11 +13,7 @@ app = FastAPI()
 app.add_middleware(
     CORSMiddleware,
     # Add both the Live Server port and the React port just in case
-    allow_origins=[
-        "http://127.0.0.1:5500", 
-        "http://localhost:5500",
-        "http://localhost:3000"
-    ],
+    allow_origins=["*"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
